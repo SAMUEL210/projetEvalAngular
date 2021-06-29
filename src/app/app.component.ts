b394c6ea27b9a7c5cd9334e7d16ccb04c44911a1
Samuel <samuel210@live.fr>
2021-06-29 13:12:17 +0200

    Exercice 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ export class AppComponent {
   title = 'projetEvaluationAngular';
   statistique1 = new Statistique(1542, "Démographie en France", "60M", "SUCCESS");
   statistique2 = new Statistique(42, "Orage en France", "150", "DANGER");
-  listStat = [this.statistique1, this.statistique2];
+  listStat = [this.statistique1, this.statistique2, new Statistique(42, " Enfant en France", "35", "SUCESS")];
 }
